b29bd3807d7a8622a7e4832415f5b4250a1abee4
Miguel Pousa Freire <put here your email>
2015-07-27 11:45:07 +0200

    initial commit

---
+++ search_by_id.py
@@ -0,0 +1,32 @@
+# SearchById gringer test
+ 
+from net.grinder.script.Grinder import grinder
+from net.grinder.script import Test
+from net.grinder.plugin.http import HTTPRequest
+from HTTPClient import NVPair
+ 
+searchByIdTest = Test(1, "Test SearchById")
+ 
+json1 = '''{\"ProductId\":\"123\"}'''
+ 
+class TestRunner:
+    def __call__(self):
+        if grinder.runNumber > 0 or grinder.threadNumber > 0:
+            raise RuntimeError("Use limited to one thread, one run; "
+                               "see Amazon Web Services terms and conditions")
+ 
+        request = HTTPRequest(url="http://api-mirror.wide-eyes.it")
+        searchByIdTest.record(request)
+ 
+        parameters = (
+            NVPair("v", "1.0"),
+            NVPair("f", "xml"),
+            NVPair("t", "webservices-20"),
+            NVPair("dev-t", "<insert license key here>"),
+            NVPair("type", "heavy"),
+            NVPair("AsinSearch", "1904284000"),
+            )
+ 
+        bytes = request.POST('/v1/SearchById', json1, ( NVPair('Content-Type', 'application/json'),  NVPair('apikey', 'helloworld'), )).inputStream
+ 
+        grinder.logger.info(str(bytes))
