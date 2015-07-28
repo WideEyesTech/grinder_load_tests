6cce8550b6115efb61b4dcf754122a773935d169
Miguel Pousa Freire <put here your email>
2015-07-28 14:02:40 +0200

    fix mistake in Search By Id

--- search_by_id.py
+++ search_by_id.py
@@ -7,7 +7,13 @@ from HTTPClient import NVPair
  
 searchByIdTest = Test(1, "Test SearchById")
  
-json1 = '''{"ProductId":"123"}'''
+json1 = '''{"ProductId":123}'''
+
+static String convertStreamToString(java.io.InputStream is) {
+    java.util.Scanner s = new java.util.Scanner(is).useDelimiter("\\A");
+    return s.hasNext() ? s.next() : "";
+}
+
  
 class TestRunner:
     def __call__(self):
@@ -15,5 +21,7 @@ class TestRunner:
         searchByIdTest.record(request)
  
         bytes = request.POST('/v1/SearchById', json1, ( NVPair('Content-Type', 'application/json'),  NVPair('apikey', 'helloworld'), )).inputStream
- 
-        grinder.logger.info(str(bytes))
+
+        s = new java.util.Scanner(bytes).useDelimiter("\\A");
+        result = s.hasNext() ? s.next() : "";
+        grinder.logger.info(str(result))
